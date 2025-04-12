65022e13cc1476e9f162c6f4af52f1cd0a163e27
PavanSabani <pavansabani12@gmail.com>
2025-04-12 18:51:26 +0530

    Profile pic Update

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { genrateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
+import cloudinary from '../lib/cloudinary.js'
 
 export const signup = async (req,res)=>{
 
@@ -91,6 +92,50 @@ export const logout = (req,res)=>{
         res.status(200).json({message:"logged out succesfully"})
     } catch (error) {
         console.log("error in logout controller",error.message);
+        res.status(500).json({message:"Internal Server Error"})
+    }
+}
+
+export const updateProfile = async (req, res) => {
+    try {
+        // getting pic from user 
+        const { profilePic } = req.body;
+
+        // validating input
+        if (!profilePic) {
+            return res.status(400).json({ message: "Profile Pic Required" });
+        }
+
+        // check if user is authenticated and exists
+        if (!req.User || !req.User._id) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        const userId = req.User._id;
+
+        // storing in cloudinary bucket 
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+        // updating db with updated pic 
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            { profilePic: uploadResponse.secure_url },
+            { new: true }
+        );
+
+        res.status(200).json(updatedUser);
+
+    } catch (error) {
+        console.error("Error in update profile:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export const checkAuth = (req,res)=>{
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller ",error.message);
         res.status(500).json({message:"Internal Server Error"})
     }
 }
