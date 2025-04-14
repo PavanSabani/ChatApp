682a389b80c70c1e2eeecd53236125c1e838501e
PavanSabani <pavansabani12@gmail.com>
2025-04-14 18:22:49 +0530

    Side bar for chat added

--- backend/src/Index.js
+++ backend/src/Index.js
@@ -25,7 +25,7 @@ app.use(cors({
 
 
 app.use("/api/auth",authRoutes)
-app.use("/api/message",MessageRoutes)
+app.use("/api/messages",MessageRoutes)
 
 app.listen(PORT,()=>{
     console.log("Server running on "+PORT);
